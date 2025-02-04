21044d685689659800d951aef0c162b6b29ff6a1
SimonaVel <simona.vel719@gmail.com>
2025-02-04 19:48:53 +0200

    Repository layer for Defense and Review

---
+++ src/main/java/com/project/diplomation/data/repositories/ReviewRepo.java
@@ -0,0 +1,15 @@
+package com.project.diplomation.data.repositories;
+
+import com.project.diplomation.data.models.entities.Review;
+import com.project.diplomation.data.models.entities.Student;
+import com.project.diplomation.data.models.entities.UniversityTutor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public interface ReviewRepo extends JpaRepository<Review, Long> {
+    List<Review> findByReviewer(UniversityTutor reviewer);
+    Review findByPassed(boolean isPassed);
+    Review findByDateOfSubmission(LocalDate date);
+}
